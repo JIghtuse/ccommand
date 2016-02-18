208ccbfb13aa68d8efe1a2418351de3032d2f42f
Boris Egorov <egorov@linux.com>
2016-02-19 01:33:29 +0600

    sh.cxx: Use idiomatic C++

--- examples/sh.cxx
+++ examples/sh.cxx
@@ -1,47 +1,71 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <stdexcept>
 #include <ccommand.h>
 
-using namespace std;
-
-// system()-like function
-void system_ccommand(const string& s)
-{
-    int ret;
-    stringstream iss(s);
-
-    string name;
-    iss >> name;
-
-    struct ccommand cmd;
-
-    ret = ccommand_init(&cmd, name.c_str());
-    if (ret < 0) goto out;
-
-    for (string arg; iss >> arg;) {
-        ret = ccommand_add_arg(&cmd, arg.c_str());
-        if (ret < 0) goto out;
+class Command {
+public:
+    Command(const std::string& name)
+        : cmd{}
+    {
+        auto ret = ccommand_init(&cmd, name.c_str());
+        if (ret < 0)
+            throw std::runtime_error{ "cannot instantiate command" };
     }
 
-    if (ccommand_exec(&cmd) < 0)
-        cerr << "error: failed to execute '" << s << "'\n";
+    ~Command()
+    {
+        ccommand_cleanup(&cmd);
+    }
 
-out:
-    ccommand_cleanup(&cmd);
+    void addArg(const std::string& arg)
+    {
+        auto ret = ccommand_add_arg(&cmd, arg.c_str());
+        if (ret < 0)
+            throw std::runtime_error{ "cannot add argument" };
+    }
+
+    void exec()
+    {
+        if (ccommand_exec(&cmd) < 0)
+            throw std::runtime_error{ "execution error" };
+    }
+
+private:
+    ccommand cmd;
+};
+
+// system()-like function
+void execute_command(const std::string& s)
+{
+    std::string name;
+    std::stringstream iss(s);
+    iss >> name;
+
+    try {
+        Command command{ name };
+        for (std::string arg; iss >> arg;)
+            command.addArg(arg);
+        command.exec();
+    }
+    catch (std::runtime_error& e) {
+        std::cerr << "failed to execute '" << s << "': " << e.what() << '\n';
+    }
 }
 
 // Dumb shell
 int main()
 {
-    const string prompt{"$ "};
-    const string exit{"exit"};
+    const std::string prompt{ "$ " };
+    const std::string exit{ "exit" };
 
-    cout << prompt;
-    for (string s; getline(cin, s);) {
+    std::cout << prompt;
+    for (std::string s; std::getline(std::cin, s);) {
         if (s == exit)
             break;
-        system_ccommand(s);
-        cout << prompt;
+        execute_command(s);
+        std::cout << prompt;
     }
+    std::cout << exit << std::endl;
 }
